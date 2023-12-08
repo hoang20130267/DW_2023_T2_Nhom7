9b237bc3a71a2d5bd01066f91671fad2f66194af
Nguyễn Hữu Đạt <20130224@st.hcmuaf.edu.vn>
2023-12-09 02:17:20 +0700

    Revert "Revert "update transform db staging to xoso_dw by table bảng dim""
    This reverts commit d2a9e12d4cf326cedec4bf807ae2b6888b284291.

--- src/main/java/Bean/Staging.java
+++ src/main/java/Bean/Staging.java
@@ -1,36 +1,29 @@
 package Bean;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Staging {
-    private int id;
     private String prize;
     private String province;
     private String domain;
     private String number_winning;
     private String date;
-    private String date_update;
+    private String date_updated;
     private String date_expired;
 
-    public Staging(int id, String prize, String province, String domain, String number_winning, String date, String date_update, String date_expired) {
-        this.id = id;
+    public Staging() {
+    }
+
+    public Staging(String prize, String province, String domain, String number_winning, String date, String date_updated, String date_expired) {
         this.prize = prize;
         this.province = province;
         this.domain = domain;
         this.number_winning = number_winning;
         this.date = date;
-        this.date_update = date_update;
+        this.date_updated = date_updated;
         this.date_expired = date_expired;
-    }
-
-    public Staging() {
-
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
     }
 
     public String getPrize() {
@@ -73,12 +66,12 @@ public class Staging {
         this.date = date;
     }
 
-    public String getDate_update() {
-        return date_update;
+    public String getDate_updated() {
+        return date_updated;
     }
 
-    public void setDate_update(String date_update) {
-        this.date_update = date_update;
+    public void setDate_updated(String date_updated) {
+        this.date_updated = date_updated;
     }
 
     public String getDate_expired() {
@@ -92,14 +85,27 @@ public class Staging {
     @Override
     public String toString() {
         return "Staging{" +
-                "id=" + id +
-                ", prize='" + prize + '\'' +
+                "prize='" + prize + '\'' +
                 ", province='" + province + '\'' +
                 ", domain='" + domain + '\'' +
                 ", number_winning='" + number_winning + '\'' +
                 ", date='" + date + '\'' +
-                ", date_update='" + date_update + '\'' +
+                ", date_updated='" + date_updated + '\'' +
                 ", date_expired='" + date_expired + '\'' +
                 '}';
+    }
+
+    public void calculateDates() {
+        SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy");
+        try {
+            Date parsedDate = sdf.parse(this.date);
+            // Set date_updated
+            this.date_updated = sdf.format(parsedDate);
+            // Set date_expired
+            long millis = parsedDate.getTime() + (30 * 24 * 60 * 60 * 1000L);
+            this.date_expired = sdf.format(new Date(millis));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
     }
 }
