719a59d221a4e35b0f546009598616cb2eda1aa7
Nguyễn Hữu Đạt <20130224@st.hcmuaf.edu.vn>
2023-12-19 21:59:43 +0700

    fix code sendmail và thêm ngoại lệ
    fix lỗi thư viện

--- src/main/java/DAO/SendEmail.java
+++ src/main/java/DAO/SendEmail.java
@@ -1,9 +1,6 @@
 package DAO;
 
-import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.PasswordAuthentication;
-import javax.mail.Session;
+import javax.mail.*;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 import java.util.Properties;
@@ -11,17 +8,16 @@ import java.util.Properties;
 public class SendEmail {
     public static void sendMailError(String content) {
         String fromEmail = "20130224@st.hcmuaf.edu.vn";
-        String password = "";
+        String password = "HuuBi19/12";
         String toEmail = "nguyenhwdat1912@gmail.com";
 
-
         Properties properties = new Properties();
-        properties.put("mail.smtp.host", "smtp.gmail.com"); //SMTP Host
-        properties.put("mail.smtp.port", "587"); //TLS Port
-        properties.put("mail.smtp.auth", "true"); //enable authentication
-        properties.put("mail.smtp.starttls.enable", "true"); //enable STARTTLS
+        properties.put("mail.smtp.host", "smtp.gmail.com"); // SMTP Host
+        properties.put("mail.smtp.port", "587"); // TLS Port
+        properties.put("mail.smtp.auth", "true"); // enable authentication
+        properties.put("mail.smtp.starttls.enable", "true"); // enable STARTTLS
 
-        Session session = Session.getDefaultInstance(properties, new javax.mail.Authenticator() {
+        Session session = Session.getInstance(properties, new javax.mail.Authenticator() {
             protected PasswordAuthentication getPasswordAuthentication() {
                 return new PasswordAuthentication(fromEmail, password);
             }
@@ -30,9 +26,13 @@ public class SendEmail {
         try {
             MimeMessage message = new MimeMessage(session);
             message.setFrom(new InternetAddress(fromEmail));
-            message.addRecipients(Message.RecipientType.TO, InternetAddress.parse(toEmail, false));
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(toEmail));
             message.setSubject("Thông báo lỗi");
-            message.setContent(content, "text/plain");
+            message.setText(content);
+
+            // Send the message
+            Transport.send(message);
+
             System.out.println("Gửi mail thành công");
         } catch (MessagingException e) {
             throw new RuntimeException(e);
